6654b3e90f7195691a733a5c0e42fdb18b18d0d9
megan <meganyaur2022@u.northwestern.edu>
2022-10-05 01:42:56 -0500

    term filter

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,47 @@ import Banner from './components/Banner';
 import CourseList from './components/CourseList';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useJsonQuery } from './utilities/fetch';
+import { useState } from 'react';
 
 const queryClient = new QueryClient();
 
+const terms = {
+  Fall: 'Fall',
+  Winter: 'Winter',
+  Spring: 'Spring'
+};
+
+const TermButton = ({term, selection, setSelection}) => (
+  <div>
+    <input type="radio" id={term} className="btn-check" checked={term === selection} autoComplete="off"
+      onChange={() => setSelection(term)} />
+    <label className="btn btn-success mb-1 p-2" htmlFor={term}>
+    { term }
+    </label>
+  </div>
+);
+
+const TermSelector = ({selection, setSelection}) => (
+  <div className="btn-group">
+    { 
+      Object.keys(terms).map(term => <TermButton key={term} term={term} selection={selection} setSelection={setSelection} />)
+    }
+  </div>
+);
+
+const TermPage = ({courses}) => {
+  const [selection, setSelection] = useState(() => Object.keys(terms)[0]);
+  return (
+    <div>
+      <TermSelector selection={selection} setSelection={setSelection} />
+      <CourseList courses={courses} term={selection}/>
+    </div>
+  );
+}
+
 const Main = () => {
   const [schedule, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+  const [selection, setSelection] = useState(() => Object.keys(terms)[0]);
 
   if (error) return <h1>Error loading user data: {`${error}`}</h1>;
   if (isLoading) return <h1>Loading user data...</h1>;
@@ -17,7 +53,7 @@ const Main = () => {
 
   return  <div className="container">
             <Banner title={schedule.title}/>
-            <CourseList courses = {schedule.courses}/>
+            <TermPage courses={schedule.courses}></TermPage>
           </div>;
 }
 
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,20 +1,23 @@
 import './CourseList.css';
 
-const CourseList = ({courses}) => {
-    let courseKeys = Object.keys(courses)
-    return <div className="CardGroup">
-        {courseKeys.map((course) => <div key={course} className='card m-1 p-2'>
-            <div className='top'>
-                <h5>{courses[course].term} CS {courses[course].number}</h5>
-                <p>{courses[course].title}</p>
+const CourseList = ({courses, term}) => {
+    return (
+        <div className="CardGroup"> {
+            Object.entries(courses).filter(course => course[1].term === term).map(([id, course]) =>
+            <div key={id} className='card m-1 p-2'>
+                <div className='top'>
+                    <h5>{course.term} CS {course.number}</h5>
+                    <p>{course.title}</p>
+                </div>
+                <hr></hr>
+                <div className="card-body">
+                    <p>{course.meets}</p>
+                </div>
             </div>
-            <hr></hr>
-            <div className="card-body">
-                <p>{courses[course].meets}</p>
-            </div>
-        </div>)}
-    </div>
+            )
+            }
+        </div>
+    )
 };
 
 export default CourseList;
-
