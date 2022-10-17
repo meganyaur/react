9d2069960fb19773fb054ccae411e46edc534590
megan <meganyaur2022@u.northwestern.edu>
2022-10-16 23:54:33 -0500

    database

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Banner from './components/Banner';
 import TermPage from './components/TermPage';
 import CourseFormPage from './components/CourseFormPage'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useJsonQuery } from './utilities/fetch';
+import { useDbData } from './utilities/firebase';
 import { addConflicts, removeConflicts } from "./utilities/conflict";
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
@@ -31,11 +31,8 @@ const Main = () => {
       setSelected([...selected, course]);
     }
   };
-  const [schedule, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
-
-  if (error) return <h1>Error loading user data: {`${error}`}</h1>;
-  if (isLoading) return <h1>Loading user data...</h1>;
-  if (!schedule) return <h1>No user data found</h1>;
+  const [schedule, error] = useDbData("/");
+  console.log(schedule)
 
   return  <div className="container">
             <Banner title={schedule.title}/>
